7664332d4355eb07ada995fd937603998544f20f
anshuUnity <64951054+anshuUnity@users.noreply.github.com>
2021-06-29 16:50:35 +0530

    Update settings.py

--- blog_intern/settings.py
+++ blog_intern/settings.py
@@ -50,6 +50,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = '/accounts/login'
 
